ec182bdb51f32f95f4bb13b238f9844de63f8e1c
Zichen Zheng <zzchencn@gmail.com>
2013-11-27 14:26:15 -0500

    Environment setup; hello world code

--- GestureDetector/main.cpp
+++ GestureDetector/main.cpp
@@ -6,13 +6,27 @@
 //  Copyright (c) 2013 Zichen Zheng. All rights reserved.
 //
 
-#include <iostream>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
 
-int main(int argc, const char * argv[])
-{
+using namespace cv;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int, char**) {
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+    
+    Mat edges;
+    namedWindow("edges",1);
+    while(true) {
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
-
