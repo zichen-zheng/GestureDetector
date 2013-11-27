07fc4333a3ee2337036da365b4de974a4efaa03f
Zichen Zheng <zzchencn@gmail.com>
2013-11-27 13:49:49 -0500

    Initial Commit

---
+++ GestureDetector/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GestureDetector
+//
+//  Created by Zichen Zheng on 11/27/13.
+//  Copyright (c) 2013 Zichen Zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
