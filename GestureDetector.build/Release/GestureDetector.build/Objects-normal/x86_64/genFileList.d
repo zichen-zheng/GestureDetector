6484935187e33c0087c019eb295bcb381c6b843a
Zichen Zheng <zzchencn@gmail.com>
2013-12-03 20:09:30 -0500

    Clean up

--- GestureDetector.build/Release/GestureDetector.build/Objects-normal/x86_64/genFileList.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Zichen/Documents/Workspace/cs6737-biometrics/GestureDetector/GestureDetector/util/genFileList.cpp \
-  /Users/Zichen/Documents/Workspace/cs6737-biometrics/GestureDetector/GestureDetector/util/genFileList.h
